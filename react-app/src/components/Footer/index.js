7d3f6b68e032f376353ddfeb3a1708a1fdb020b7
Hayden Gogan <h.gogan03@icloud.com>
2024-01-17 13:05:59 -0800

    removed dead linkedin variables

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,24 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  const handleLinkedinMira = (e) => {
-    e.preventDefault();
-    window.open("https://www.linkedin.com/in/miroslawa-borkowska-3b72332a0/");
-  };
-
   const handleLinkedinHayden = (e) => {
     e.preventDefault();
     window.open("https://www.linkedin.com/in/hayden-gogan-2570a92a1/");
-  };
-
-  const handleLinkedinJosh = (e) => {
-    e.preventDefault();
-    window.open("https://www.linkedin.com/in/josh-goldenberg-252416a1/");
-  };
-
-  const handleLinkedinAlex = (e) => {
-    e.preventDefault();
-    window.open("https://www.linkedin.com/in/alexander-heasley-1979732a0/");
   };
 
   return (
